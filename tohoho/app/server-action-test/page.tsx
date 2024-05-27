7ea0f27b6a6440c03ea8c75f8c09ad5dfc2246bc
Naoaki Obiki <obiki.wk@gmail.com>
2024-05-27 17:17:47 +0900

    server action

---
+++ tohoho/app/server-action-test/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  async function serverside_action_test(form) {
+    'use server' /* 'use server' を指定したサーバサイドアクションを、クライアント側のフォームから呼び出す */
+    const message = form.get('message')
+    console.log({message})
+  }
+  return (
+    <>
+      <h1>Server Action Test</h1>
+      <form action={serverside_action_test}>
+        <input type="text" name="message" />
+        <button type="submit">OK</button> {/* クライアントのフォームに入力したメッセージがサーバに送られ、サーバのコンソールに出力される */}
+      </form>
+    </>
+  )
+}
