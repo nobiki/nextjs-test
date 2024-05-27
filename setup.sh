eef9cc327b6763583fcff03e1aba975f7ffcf66a
Naoaki Obiki <obiki.wk@gmail.com>
2024-05-27 15:44:33 +0900

    Tailwind CSS

--- setup.sh
+++ setup.sh
@@ -8,12 +8,16 @@ echo 22.2.0 > .node-version
 nodenv install
 node -v
 
+## Download .gitignore (needs curl)
+curl -o .gitignore https://raw.githubusercontent.com/github/gitignore/main/Node.gitignore
+
 ## Install Next.js
 npm install next@latest react@latest react-dom@latest typescript@latest @types/react @types/node
 mkdir app
 
-## Download .gitignore (needs curl)
-curl -o .gitignore https://raw.githubusercontent.com/github/gitignore/main/Node.gitignore
+## Install Tailwind CSS
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
 
 ## add Scripts to package.json (needs jq)
 cat package.json | jq '. |= .+{"scripts": {"dev":"next dev","build":"next build","start":"next start","lint":"next lint"}}' > package.json.new
