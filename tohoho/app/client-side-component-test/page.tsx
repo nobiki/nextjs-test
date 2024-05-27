615785c8cc3e97a16a31e473a4479cdba92d0076
Naoaki Obiki <obiki.wk@gmail.com>
2024-05-27 17:06:41 +0900

    Server/Client Side Component

---
+++ tohoho/app/client-side-component-test/page.tsx
@@ -0,0 +1,5 @@
+'use client'
+export default function Page() {
+  console.log('Client Side Test')
+  return <h1>Client Side Test</h1>
+}
---
+++ tohoho/app/server-side-component-test/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log('Server Side Test')
+  return <h1>Server Side Test</h1>
+}
