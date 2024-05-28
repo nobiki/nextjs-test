0f380e51bca9acd87da844759c8c0179e3921c8f
Naoaki Obiki <obiki.wk@gmail.com>
2024-05-28 14:38:41 +0900

    form

---
+++ tohoho/app/api/form-test/route.ts
@@ -0,0 +1,6 @@
+export async function POST(request: Request) {
+  const formData = await request.formData()
+  const name = formData.get('name')
+  const email = formData.get('email')
+  return Response.json({ name, email })
+}
